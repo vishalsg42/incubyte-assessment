4b7515f6cc7f504b8618bdf5736af20cfb436d7e
Vishal Gupta <vishalsg42@gmail.com>
2025-01-11 18:07:01 +0530

    Handling Multiple Numbers

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -8,3 +8,6 @@ def test_single_number_returns_its_value():
 
 def test_two_numbers_returns_their_sum():
     assert add("1,2") == 3
+
+def test_multiple_numbers_returns_their_sum():
+    assert add("1,2,3,4") == 10
